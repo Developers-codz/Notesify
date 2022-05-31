6b0b6ad4b4de0d583c43a5ffff0915483e723256
developers-codz <kaj123al2002@gmail.com>
2022-05-31 12:42:52 +0530

    feat: api call for create note

--- src/Redux/Reducers/notesSlice.js
+++ src/Redux/Reducers/notesSlice.js
@@ -2,7 +2,8 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 const initialState = {
   userProfile: {},
-  modalOpen:false
+  modalOpen: false,
+  notes:[]
 };
 
 const getUserProfile = createAsyncThunk(
@@ -19,28 +20,54 @@ const getUserProfile = createAsyncThunk(
           },
         }
       );
-      console.log(response.data)
+      console.log(response.data);
     } catch (error) {
       rejectWithValue(error);
     }
   }
 );
+
+export const postNoteHandler = createAsyncThunk("notes/postNoteHandler", async (note,{rejectWithValue}) => {
+  const encodedToken = localStorage.getItem("token");
+  try {
+    const response = await axios.post(
+      "/api/notes",
+      {note},
+      {
+        headers: {
+          authorization: encodedToken,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    rejectWithValue(error)
+  }
+});
+
 const notesSlice = createSlice({
   name: "notes",
   initialState,
-  reducers:{
-    handleToggleModal: (state,action) =>{
-        state.modalOpen = !state.modalOpen
-    }
+  reducers: {
+    handleToggleModal: (state, action) => {
+      state.modalOpen = !state.modalOpen;
+    },
   },
-  extraReducers(builder){
-      builder.addCase(getUserProfile.fulfilled,(state,action)=>{
-          state.userProfile = action.payload;
-          console.log(action.payload)
-      })
-  }
+  extraReducers(builder) {
+    builder.addCase(getUserProfile.fulfilled, (state, action) => {
+      state.userProfile = action.payload;
+      console.log(action.payload);
+    });
+
+    builder.addCase(postNoteHandler.fulfilled,(state,action) =>{
+      console.log(action.payload)
+      state.notes = action.payload
+    })
+    builder.addCase(postNoteHandler.rejected,(state,action)=>{
+      console.log(action.payload)
+    })
+  },
 });
 export default notesSlice.reducer;
-const { actions } =notesSlice;
-export const {handleToggleModal} = actions;
-
+const { actions } = notesSlice;
+export const { handleToggleModal } = actions;
--- src/components/modal/EditorModal.jsx
+++ src/components/modal/EditorModal.jsx
@@ -1,7 +1,8 @@
-import React , { useState }from 'react'
+import React, { useState,useRef } from "react";
 import "../../pages/home/home.css";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import parse from "html-react-parser";
 import {
   EditorWrapper,
   TitleBox,
@@ -13,62 +14,94 @@ import {
   WhiteButton,
   ButtonToNote,
   CloseButton,
-  EditorFooter
+  EditorFooter,
 } from "../../pages/home/homeComponents";
-import {handleToggleModal} from "../../Redux/Reducers/notesSlice"
+
+import { handleToggleModal ,postNoteHandler} from "../../Redux/Reducers/notesSlice";
 import { useDispatch } from "react-redux";
 
-
-
 const modules = {
-    toolbar: [
-      [{ font: [] }],
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [{ size: ["small", false, "large", "huge"] }],
-      [{ list: "ordered" }, { list: "bullet" }],
-      ["link", "image"],
-      [{ color: [] }, { align: [] }],
-    ],
-  };
+  toolbar: [
+    [{ font: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{ size: ["small", false, "large", "huge"] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["link", "image"],
+    [{ color: [] }, { align: [] }],
+  ],
+};
+
+const noModules = {
+  toolbar: false,
+};
+
 export const EditorModal = () => {
-    const dispatch = useDispatch();
-    const [bgColor, setbgColor] = useState("white");
+  const dispatch = useDispatch();
+
+  const [bgColor, setbgColor] = useState("white");
+  const [note, setNote] = useState({ title: "", content: "" });
+  const [enable, setEnable] = useState(true);
+
+  const inputRef = useRef();
+
+  const changeHandler = (e) => {
+    setNote((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const clickHandler = () => {
+    setEnable(false)
+  const parsedData = parse(`${note.content}`).props.children
+    dispatch(postNoteHandler({title:note.title,content:parsedData}))
+    dispatch(handleToggleModal())
+
+  };
+
   return (
     <>
-    <EditorWrapper style={{ backgroundColor: bgColor }}>
-      
-      <CloseButton onClick={()=>dispatch(handleToggleModal())}>  X</CloseButton>
-  
+      <EditorWrapper style={{ backgroundColor: bgColor }}>
+        <CloseButton onClick={() => dispatch(handleToggleModal())}>
+          {" "}
+          X
+        </CloseButton>
 
-    <TitleBox
-      type="text"
-      placeholder="Add title ....."
-      style={{ backgroundColor: bgColor }}
-    />
-    <ReactQuill
-      modules={modules}
-      placeholder={"Add notes......"}
-      height={"100px"}
-    />
-    <EditorFooter>
-    <Pallette>
-      <PinkButton onClick={() => setbgColor("lightpink")}></PinkButton>
-      <BlueButton onClick={() => setbgColor("lightblue")}></BlueButton>
-      <GreenButton onClick={() => setbgColor("lightgreen")}></GreenButton>
-      <YellowButton onClick={() => setbgColor("yellow")}></YellowButton>
-      <WhiteButton onClick={() => setbgColor("white")}></WhiteButton>
-    </Pallette>
-    <div>
-  <select>
-    <option>Priority</option>
-    <option>Low</option>
-    <option>High</option>
-    <option>Medium</option>
-  </select>
-    <ButtonToNote>Add</ButtonToNote>
-    </div>
-    </EditorFooter>
-  </EditorWrapper>
+        <TitleBox
+          type="text"
+          placeholder="Add title ....."
+          style={{ backgroundColor: bgColor }}
+          value={note.title}
+          name="title"
+          onChange={changeHandler}
+        />
+        <ReactQuill
+          placeholder={"Add notes......"}
+          modules={enable ? modules : noModules}
+          height={"100px"}
+          value={note.content}
+          readOnly={!enable}
+          ref={inputRef}
+          onChange={(e) =>
+            setNote((prev) => ({ ...prev, content: e }))
+          }
+        />
+        <EditorFooter>
+          <Pallette>
+            <PinkButton onClick={() => setbgColor("lightpink")}></PinkButton>
+            <BlueButton onClick={() => setbgColor("lightblue")}></BlueButton>
+            <GreenButton onClick={() => setbgColor("lightgreen")}></GreenButton>
+            <YellowButton onClick={() => setbgColor("yellow")}></YellowButton>
+            <WhiteButton onClick={() => setbgColor("white")}></WhiteButton>
+          </Pallette>
+          <div>
+            <select>
+              <option>Priority</option>
+              <option>Low</option>
+              <option>High</option>
+              <option>Medium</option>
+            </select>
+            <ButtonToNote onClick={clickHandler}>Add</ButtonToNote>
+          </div>
+        </EditorFooter>
+      </EditorWrapper>
     </>
-  )
-}
+  );
+};
