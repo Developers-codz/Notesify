7030d89f07f558064acf5f48109f1978cb50a63c
developers-codz <kaj123al2002@gmail.com>
2022-06-04 14:05:42 +0530

    fix: title trash

--- src/pages/trash/Trash.jsx
+++ src/pages/trash/Trash.jsx
@@ -8,8 +8,10 @@ import {
 import { TrashIcon } from "../../assets/icons";
 import { Link } from "react-router-dom";
 import { EachNote } from "../../components";
+import {useDocumentTitle} from "../../functions"
 
 export const Trash = () => {
+  useDocumentTitle("Trash")
   const { trash } = useSelector((store) => store.notes);
   const [isLoading, setLoading] = useState(true);
   useEffect(() => {
